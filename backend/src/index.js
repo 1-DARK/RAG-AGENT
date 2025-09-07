d59e55bf29bfe692fabfcba07fe567501ccc8352
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-09-07 13:28:36 +0530

    backend done

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
-import { authroutes } from "./routes/auth.route.js";
+import authRoutes from "./routes/auth.route.js";
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 const app = express();
-app.use("/api/auth", authroutes);
+app.use("/api/auth", authRoutes);
 app.listen(PORT, () => {
   console.log("Server started at http://localhost:" + PORT);
   connectDB();
