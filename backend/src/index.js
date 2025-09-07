5e740b16667befe100363f640f8c01795463cf54
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-09-07 19:31:57 +0530

    initial commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,26 @@
 import express from "express";
-import dotenv from "dotenv";
-import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import cors from "cors";
+import { connectDB } from "./lib/db.js";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import { app, server } from "./lib/socket.js";
+
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
-const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use("/api/auth", authRoutes);
-app.listen(PORT, () => {
+app.use("/api/messages", messageRoutes);
+server.listen(PORT, () => {
   console.log("Server started at http://localhost:" + PORT);
   connectDB();
 });
